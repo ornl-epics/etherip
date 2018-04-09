e88e1c479b6798d7ec08be4610db81497c82cc03
Samuel Pelletier <samuel@kaviju.com>
2018-04-09 16:43:34 -0400

    Add some AB Logix 5000 constants.

--- src/etherip/types/CNService.java
+++ src/etherip/types/CNService.java
@@ -18,17 +18,25 @@ package etherip.types;
 public enum CNService
 {
     Get_Attribute_All(0x01),
+    Get_Attributes_List(0x03), // Logix5000 Data Access
     Get_Attribute_Single(0x0E), // Spec 6 p. 43
     CIP_MultiRequest(0x0A), // Logix5000 Data Access
     CIP_ReadData(0x4C), // Logix5000 Data Access
+    CIP_ReadDataFragmented(0x52), // Logix5000 Data Access
     CIP_WriteData(0x4D), // Logix5000 Data Access
+    CIP_WriteDataFragmented(0x53), // Logix5000 Data Access
     CM_Unconnected_Send(0x52),
+    Get_Instance_Attribute_List(0x55), // Logix5000 Data Access
     Get_Connection_Data(0x56), // CIP VOL1_3.3: 3-5.5
     Get_Attribute_All_Reply(0x01 | 0x80),
+    Get_Attributes_List_Reply(0x03 | 0x80), // Logix5000 Data Access
     Get_Attribute_Single_Reply(0x0E | 0x80),
     CIP_MultiRequest_Reply(0x0A | 0x80),
     CIP_ReadData_Reply(0x4C | 0x80),
+    CIP_ReadDataFragmented_Reply(0x52 | 0x80),
     CIP_WriteData_Reply(0x4D | 0x80),
+    CIP_WriteDataFragmente_Reply(0x53 | 0x80), // Logix5000 Data Access
+    Get_Instance_Attribute_List_Reply(0x55 | 0x80), // Logix5000 Data Access
     CM_Unconnected_Send_Reply(0x52 | 0x80);
 
     final private byte code;
